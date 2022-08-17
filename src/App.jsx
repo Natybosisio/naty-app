3704205c46ca5134065654f7f036e4b3694551f4
NataliaBosisio <nbosisio@hotmail.com>
2022-08-17 17:50:12 -0300

    React con errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from  './components/NavBar';
 
 const App = () => {
   const persona = {nombre: "naty", descripcion: "este es mi primer proyecto con React"};
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          Soy {persona.nombre} y {persona.descripcion}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <a className="App-link"    href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+//CREAR BARRA DE MENU SIMPLE
+
+const NavBar =() =>{
+  <nav>
+    <div>
+        hola
+    </div>
+  </nav>
+}
+
+export default NavBar; 
